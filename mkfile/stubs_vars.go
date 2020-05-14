54b9e31357073e2801d8930f6348c0669c2072b2
Daniel Hess <dan9186@gmail.com>
2020-05-13 19:17:18 -0700

    add functional tests to the makefile stub

--- mkfile/stubs_vars.go
+++ mkfile/stubs_vars.go
@@ -3,6 +3,7 @@ package mkfile
 const varsStub = `SHELL = bash
 
 APP := $(shell basename $(PWD) | tr '[:upper:]' '[:lower:]')
+TTY := $(shell if [ -z $$CI ]]; then echo "-t"; fi)
 
 GIT_COMMIT_HASH ?= $(shell git rev-parse HEAD)
 GIT_SHORT_COMMIT_HASH := $(shell git rev-parse --short HEAD)`
