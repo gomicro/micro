af1f4b9da7dd27e6f5be376f7e798ecdfeb9f9f3
Daniel Hess <dan9186@gmail.com>
2020-10-27 09:54:51 -0700

    shushing makefile bits

--- mkfile/stubs_linters.go
+++ mkfile/stubs_linters.go
@@ -2,4 +2,4 @@ package mkfile
 
 const lintersStub = `.PHONY: linters
 linters: ## Run all the linters
-	docker run -v $$PWD:/go$${PWD/$$GOPATH} --workdir /go$${PWD/$$GOPATH} gomicro/golinters`
+	@docker run -v $$PWD:/go$${PWD/$$GOPATH} --workdir /go$${PWD/$$GOPATH} gomicro/golinters`
